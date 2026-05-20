440a857b666aab85792285e3de51c9d9b5595caa
Smdgameking <smdgameking@gmail.com>
2026-05-20 15:30:51 +0530

    Updated login and register system

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router();
+const User = require('./models/User');
+const bcrypt = require('bcrypt');
 const session = require('express-session');
 router.use(session({
   secret: "egcorsecret",
@@ -10,23 +12,33 @@ router.get('/auth', (req, res) => {
   res.render('create-user');
 });
 router.post('/register-user', async (req, res) => {
-  const { username, email, password, confirmpassword , captcha} = req.body;
-  if(captcha !== req.session.captcha ){
+  if (req.body.captcha !== req.session.captcha) {
     return res.send("wrong captcha");
   }
-  try {
-    await User.create({ username, email, password });
-    res.status(201).send('User created successfully');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error creating user');
+  const salt = await bcrypt.genSalt(12);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const hashedConfirmPassword = await bcrypt.hash(req.body.confirmpassword, salt);
+  const user = new User({
+    fullname: {
+      firstname: req.body.firstname,
+      lastname: req.body.lastname
+    },
+    email: req.body.email,
+    phone: req.body.phone,
+    dob: req.body.dob,
+    gender: req.body.gender,
+    qualification: req.body.qualification,
+    password: hashedPassword,
+    confirmpassword: hashedConfirmPassword
+  });
+  await user.save();
+  res.send("User registered successfully");
+});
+router.post('/login', async (req, res) => {
+  const { username, email, password, captcha } = req.body;
+  if (captcha !== req.session.captcha) {
+    return res.send("wrong captcha");
   }
 });
-router.post('/login-user', async ()=>{
-  const { username, email, password , captcha} = req.body;
-  if(captcha !== req.session.captcha ){
-    return res.send("wrong captcha");
-  }
-})
 
 module.exports = router
--- models/User.js
+++ models/User.js
@@ -1,7 +1,54 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    firstname: {
-        type: String
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength: 30
+        },
+        lastname: {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength: 30
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone:{
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ['Male', 'Female', 'Other']
+    },
+    qualification: {
+        type: String,
+        required: true,
+        enum: ['10th pass', '12th pass', 'Diploma','Degree', 'B.Tech']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 1024
+    },
+    confirmpassword: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 1024
     }
 });
 
